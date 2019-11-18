0f732584e23fb7041d20fd5f3fd954cbd8be349d
HrBjarup <cph-ab363@cphbusiness.dk>
2019-11-18 13:12:51 +0100

    Init setup for frontend. React-bootstrap and react-routing has been added in the dependencies. Removed unused files from scr folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        console.log("Test")
+        //create react components / routes here
+      }
+    </div>
+  );
+}
+
+export default App;
