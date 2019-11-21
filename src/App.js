21fa94be06e304d1c52f10dbb4939c3adc443a65
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-21 17:03:19 +0100

    Initial Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Europe from "./components/EuropeMap.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import City from "./components/City.jsx";
+import Date_ from "./components/Date.jsx";
+import Result from "./components/Result.jsx";
 
 function App() {
   return (
@@ -11,11 +15,26 @@ function App() {
         //create react components / routes here
       }
       <Header />
-      <p>Semester Project</p>
-      <Europe></Europe>
+      <Router>
+        <Switch>
+          <Route path="/city" component={City} />
+          {/* Date is a reserved Word - My VS Code was not happy */}
+          <Route path="/date" component={Date_} />
+          <Route path="/result" component={Result} />
+          <Route exact path="/" component={Europe} />
+          <Route path="*" component={NoMatch} />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
 }
+
+/**
+ * Move this to components, if it becomes more complex than this.
+ */
+const NoMatch = () => {
+  return <h1>There is nothing here.</h1>;
+};
 
 export default App;
---
+++ src/components/Date.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Date = () => {
+  return (
+    <>
+      <h1>DATE COMPONENT HERE</h1>
+      <a href="#/city">Back Button</a>
+    </>
+  );
+};
+
+export default Date;
