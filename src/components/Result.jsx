77cb035c4503bf7424dbbacbca9c4d4d4426fafc
Rúni VN <Runiniclasen@hotmail.com>
2019-11-26 21:33:43 +0100

    Push current progress

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,84 +1,175 @@
-import React, { useEffect, useState } from "react";
-import Facade from "../apiFacade";
+import React, {useEffect, useState} from 'react';
+import {Tab, Tabs, Modal, Button, Card, Badge} from 'react-bootstrap';
+import Facade from '../apiFacade';
 
-const Result = ({ startDate, endDate, country, city }) => {
-  const [data, setData] = useState([{}]);
-  const [userMessage, setUserMessage] = useState("Loading Events...");
+const Result = ({startDate, endDate, country, city}) => {
+	const [data, setData] = useState();
+	const [userMessage, setUserMessage] = useState('Loading Events...');
 
-  useEffect(() => {
-    Facade.getEvents(startDate, endDate, country, city)
-      .then(d => {
-        setData(d);
-        setUserMessage("");
-        // console.log(JSON.stringify(d)); // TODO DELETE this console.log - Was Used for testing Purposes
-        // console.log("d length = " + d.length);
-        // console.log("data length = " + data.length);
-        // console.log(data);
-      })
-      .catch(err => {
-        if (err.status) {
-          err.fullError.then(err => {
-            console.log(err);
-            setUserMessage(err.message);
-          });
-        } else {
-          console.log("Network error");
-          setUserMessage("Network Error.");
-        }
-      });
-  }, []);
-
-  return (
-    <>
-      <h1>RESULT COMPONENT HERE</h1>
-      <p>{userMessage}</p>
-      <PrintEvents data={data} />
-      <p>
-        Start Date = <ParseDate date={startDate} />
-      </p>
-      <p>
-        End Date = <ParseDate date={endDate} />
-      </p>
-      <p>Country = {country}</p>
-      <p>City = {city}</p>
-      <a href="#/date">Back Button</a>
-    </>
-  );
+	useEffect(() => {
+		Facade.getEvents(startDate, endDate, country, city)
+			.then(fetchData => {
+				console.log('fetchData:', fetchData);
+				setData(fetchData);
+				setUserMessage('');
+			})
+			.catch(err => {
+				if (err.status) {
+					err.fullError.then(err => {
+						console.log(err);
+						setUserMessage('No events for given city and date found');
+					});
+				} else {
+					console.log('Network error');
+					setUserMessage('Network Error.');
+				}
+			});
+	}, []);
+	return (
+		<>
+			{userMessage}
+			<Modal.Dialog centered>
+				{/* {userMessage && (
+					<Modal.Header>
+						<Modal.Title>{userMessage}</Modal.Title>
+					</Modal.Header>
+				)} */}
+				<Modal.Body>
+					<p>
+						Start Date = <ParseDate date={startDate} />
+					</p>
+					<p>
+						End Date = <ParseDate date={endDate} />
+					</p>
+					<p>Country = {country}</p>
+					<p>City = {city}</p>
+					{/* {data && <ControlledTabs eventData={data} />} */
+					/*Enable this if you want only to display tabs on succesful fetch */}
+					<ControlledTabs eventData={data} />
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="primary" onClick={() => window.history.back()}>
+						Go back
+					</Button>
+				</Modal.Footer>
+			</Modal.Dialog>
+		</>
+	);
 };
 
-const PrintEvents = ({ data }) => {
-  if (data.length > 0) {
-    return (
-      <>
-        {data.map(
-          ({
-            eventAddress,
-            eventDate,
-            eventName,
-            eventURL,
-            latitude,
-            longitude
-          }) => (
-            <>
-              <h3>{eventName}</h3>
-              <ul key={eventURL}>
-                <li>Event Address: {eventAddress}</li>
-                <li>Event Date: {eventDate}</li>
-              </ul>
-            </>
-          )
-        )}
-      </>
-    );
-  } else {
-    return "Something went wrong.";
-  }
+const ParseDate = ({date}) => {
+	let month = date.getMonth();
+	month = month + 1;
+	return '' + date.getFullYear() + '-' + month + '-' + date.getDate();
 };
 
-const ParseDate = ({ date }) => {
-  let month = date.getMonth();
-  month = month + 1;
-  return "" + date.getFullYear() + "-" + month + "-" + date.getDate();
+const ControlledTabs = ({eventData}) => {
+	const [key, setKey] = useState('events');
+	let listSize = 0;
+	if (eventData) listSize = Object.keys(eventData).length;
+
+	return (
+		<Tabs id="controlled-tabs" activeKey={key} onSelect={k => setKey(k)}>
+			<Tab
+				eventKey="events"
+				title={
+					<span>
+						Events <Badge variant="primary">{listSize}</Badge>
+					</span>
+				}>
+				<Events data={eventData} />
+			</Tab>
+			<Tab eventKey="weather" title="Weather">
+				<Weather data={'WeatherData'} />
+			</Tab>
+			<Tab eventKey="clothes" title="Clothes">
+				<Clothing data={'ClothingData'} />
+			</Tab>
+		</Tabs>
+	);
+};
+
+const Events = ({data}) => {
+	//console.log('data ', data[0]);
+	//console.log('url ', data[0].eventURL);
+	if (!data) {
+		return <p>No events for this selection.</p>;
+	} else if (data.length > 0) {
+		return (
+			<>
+				<Card border="light">
+					<Card.Body>
+						<Card.Title>Weather Info</Card.Title>
+						<Card.Text>
+							{data.map(
+								({
+									eventAddress,
+									eventDate,
+									eventName,
+									eventURL,
+									latitude,
+									longitude
+								}) => (
+									<>
+										<h3>{eventName}</h3>
+										<ul key={eventURL}>
+											<li>Event Address: {eventAddress}</li>
+											<li>Event Date: {eventDate}</li>
+											<li>
+												Event tickets:{' '}
+												<a
+													href={eventURL}
+													target="_blank"
+													rel="noopener noreferrer">
+													Purchase tickets
+												</a>
+											</li>
+										</ul>
+									</>
+								)
+							)}
+						</Card.Text>
+					</Card.Body>
+				</Card>
+			</>
+		);
+	} else {
+		return <p>No events found.</p>;
+	}
+};
+
+const Weather = ({data}) => {
+	return (
+		<>
+			<Card border="light">
+				<Card.Body>
+					<Card.Title>Weather Info</Card.Title>
+					<Card.Text>
+						<p>
+							Weather info (Should be returned from API once available): {data}
+						</p>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</>
+	);
+};
+
+const Clothing = ({data}) => {
+	return (
+		<>
+			<Card border="light">
+				<Card.Body>
+					<Card.Title>Clothes info</Card.Title>
+					<Card.Text>
+						<p>
+							Clothes info (Should be returned from API once available): {data}
+						</p>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</>
+	);
 };
 
 export default Result;
