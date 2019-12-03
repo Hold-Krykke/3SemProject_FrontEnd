9eb78b904f9d4fb3e73c5c652f65ca7a55f6ac64
HrBjarup <cph-ab363@cphbusiness.dk>
2019-12-03 13:58:37 +0100

    Not yet fixed

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {Tab, Tabs, Modal, Button, Card, Badge} from 'react-bootstrap';
-import Facade from '../apiFacade';
-import parseDate from '../utilities';
+import React, { useEffect, useState } from "react";
+import { Tab, Tabs, Modal, Button, Card, Badge } from "react-bootstrap";
+import Facade from "../apiFacade";
+import parseDate from "../utilities";
 
-const Result = ({startDate, endDate, country, city}) => {
+const Result = ({ startDate, endDate, country, city }) => {
   const [eventData, setEventData] = useState();
   const [weatherData, setWeatherData] = useState([]);
   const [fetchWeather, setFetchWeather] = useState(false); //trigger fetch onClick tab
-  const [userMessage, setUserMessage] = useState('Loading Events...');
+  const [userMessage, setUserMessage] = useState("Loading Events...");
 
   //For events
   useEffect(() => {
@@ -15,39 +15,43 @@ const Result = ({startDate, endDate, country, city}) => {
       .then(fetchData => {
         //console.log('fetchData:', fetchData);
         setEventData(fetchData);
-        setUserMessage('');
+        setUserMessage("");
       })
       .catch(err => {
         if (err.status) {
           err.fullError.then(err => {
             console.log(err);
-            if (err.message == 'No events for this City exists') {
-              setUserMessage('No events for given city and date found'); //backend error
+            if (err.message == "No events for this City exists") {
+              setUserMessage("No events for given city and date found"); //backend error
             } else {
-              setUserMessage('Network Error.'); //uncaught api error or bug
+              setUserMessage("Network Error."); //uncaught api error or bug
             }
           });
         } else {
-          console.log('Network error');
-          setUserMessage('Network Error.');
+          console.log("Network error");
+          setUserMessage("Network Error.");
         }
       });
   }, []);
 
   //For weather
   useEffect(() => {
+    console.log("useEffect - weather");
     if (!eventData) {
       setUserMessage("Event didn't load correctly");
       return;
     }
+    setWeatherData([]);
     for (const event in eventData) {
       if (eventData.hasOwnProperty(event)) {
         const element = eventData[event];
 
-        let dateArray = element.eventDate.split('-'); //yyyy-mm-dd format, so [0] is year, and so on
+        let dateArray = element.eventDate.split("-"); //yyyy-mm-dd format, so [0] is year, and so on
+        console.log("Date array: " + dateArray);
         Facade.getWeather(city, dateArray[0], dateArray[1], dateArray[2])
           .then(fetchData => {
-            if (fetchData) setWeatherData([...weatherData, fetchData[0]]);
+            setWeatherData([...weatherData, fetchData[0]]);
+
             //always returns array with 1 field
             // else //need some error handling, as there are cases that return an empty array. (?)
             // 	setWeatherData([
@@ -60,20 +64,20 @@ const Result = ({startDate, endDate, country, city}) => {
             if (err.status) {
               err.fullError.then(err => {
                 console.log(err);
-                if (err.message == 'No events for this City exists') {
-                  setUserMessage('No events for given city and date found'); //HERE it should be 'some dates didn't return weather data' && check on all possible err.messages
+                if (err.message == "No events for this City exists") {
+                  setUserMessage("No events for given city and date found"); //HERE it should be 'some dates didn't return weather data' && check on all possible err.messages
                 } else {
-                  setUserMessage('Network Error. (Error code #1)'); //uncaught api error or bug
+                  setUserMessage("Network Error. (Error code #1)"); //uncaught api error or bug
                 }
               });
             } else {
-              console.log('Network error');
-              setUserMessage('Network Error. (Error code #2)');
+              console.log("Network error");
+              setUserMessage("Network Error. (Error code #2)");
             }
           });
       }
     }
-  }, [eventData && fetchWeather]);
+  }, [eventData]);
 
   return (
     <>
@@ -92,7 +96,7 @@ const Result = ({startDate, endDate, country, city}) => {
           />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='primary' onClick={() => window.history.back()}>
+          <Button variant="primary" onClick={() => window.history.back()}>
             Go back
           </Button>
         </Modal.Footer>
@@ -107,52 +111,58 @@ const ControlledTabs = ({
   fetchWeather,
   setFetchWeather
 }) => {
-  const [key, setKey] = useState('events');
+  const [key, setKey] = useState("events");
   let listSize = 0;
   if (eventData) listSize = Object.keys(eventData).length;
   return (
     <Tabs
-      id='controlled-tabs'
+      id="controlled-tabs"
       activeKey={key}
       onSelect={k => {
         setKey(k);
-        if (k === 'weather') setFetchWeather(!fetchWeather);
-      }}>
+        if (k === "weather") setFetchWeather(!fetchWeather);
+      }}
+    >
       <Tab
-        eventKey='events'
+        eventKey="events"
         title={
           <span>
-            Events <Badge variant='primary'>{listSize}</Badge>
+            Events <Badge variant="primary">{listSize}</Badge>
           </span>
-        }>
+        }
+      >
         <Events data={eventData} />
       </Tab>
-      <Tab eventKey='weather' title='Weather'>
+      <Tab eventKey="weather" title="Weather">
         <Weather data={weatherData} />
       </Tab>
     </Tabs>
   );
 };
 
-const Events = ({data}) => {
+const Events = ({ data }) => {
+  let keyCounter = 0;
   if (!data) {
     return <p>No events for this selection.</p>;
   } else if (data.length > 0) {
     return (
       <>
-        <Card border='light'>
+        <Card border="light">
           <Card.Body>
             <Card.Title>Event info</Card.Title>
             <Card.Text>
               {data.map(
-                ({
-                  eventAddress,
-                  eventDate,
-                  eventName,
-                  eventURL,
-                  latitude,
-                  longitude
-                }) => (
+                (
+                  {
+                    eventAddress,
+                    eventDate,
+                    eventName,
+                    eventURL,
+                    latitude,
+                    longitude
+                  },
+                  index
+                ) => (
                   <>
                     <h3>event object data</h3>
                     <h4>{eventName}</h4>
@@ -160,11 +170,12 @@ const Events = ({data}) => {
                       <li>Event Address: {eventAddress}</li>
                       <li>Event Date: {eventDate}</li>
                       <li>
-                        Event tickets:{' '}
+                        Event tickets:{" "}
                         <a
                           href={eventURL}
-                          target='_blank'
-                          rel='noopener noreferrer'>
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
                           Purchase tickets
                         </a>
                       </li>
@@ -182,14 +193,14 @@ const Events = ({data}) => {
   }
 };
 
-const Weather = ({data}) => {
+const Weather = ({ data }) => {
   //console.log('WeatherDataInWeatherComponent: ', data);
   if (!data) {
     return <p>No weather info available for this selection.</p>;
   } else if (data.length > 0) {
     return (
       <>
-        <Card border='light'>
+        <Card border="light">
           <Card.Body>
             <Card.Title>Weather Info</Card.Title>
             <Card.Text>
@@ -215,8 +226,8 @@ const Weather = ({data}) => {
                       <li>predictability: {predictability}</li>
                       <li>temp: {temp}</li>
                       <li>
-                        weatherIcon:{' '}
-                        <img src={weatherIcon} height='25' width='25'></img>
+                        weatherIcon:{" "}
+                        <img src={weatherIcon} height="25" width="25"></img>
                       </li>
                       <li>weatherStatus: {weatherStatus}</li>
                       <li>windDirection: {windDirection}</li>
