69e76f03bb49a529e43c8b40a7c3d72e18048a37
aborkhovych <a.borkhovych@gmail.com>
2019-12-03 18:25:21 +0200

    linear gradient for map

--- src/components/MapViewer.jsx
+++ src/components/MapViewer.jsx
@@ -74,7 +74,7 @@ const MapViewer = props => {
             id="svg2"
             baseProfile="tiny"
 
-            fill="#c0c0c0"
+            fill="url(#gradient)"
             stroke="black"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -90,7 +90,12 @@ const MapViewer = props => {
             width="1000"
 
             xmlns="http://www.w3.org/2000/svg">
-
+            <defs>
+                <linearGradient id="gradient" x1="18%" y1="100%" x2="82%" y2="0%" gradientUnits="userSpaceOnUse">
+                    <stop offset="37%" cssText="stop-color:rgb(19,67,115);stop-opacity:1" />
+                    <stop offset="100%" cssText="stop-color:rgb(66,194,210);stop-opacity:1" />
+                </linearGradient>
+            </defs>
             <path d="M654.7 528.1l0.5 0.4 2 2.9 1.4 0.5 1.9 1.3 1.4 3.2 0.1 2.2-0.5 2.6 0.3 2.1-0.8 0.8 0.7 2 0.2 1.9 1.2 2.2 1.2 1.1 1.3 2.4 1.6-0.2 1.3 1.1 0 1.1 1.1 1.8-0.8 2.6-1.7 0.8-1.2 3.1-0.3 2-0.6 0.5-1.9 0.3-1.7 1.3 1 2.2-0.9 0.7-0.3 1.5-0.7 0.7-2.7-0.9-0.7-2.5-1.7-2.7-4.9-2.6-1.2-1.1 0.4-1.5-0.1-1.4-1.4-2.4 0.3-2.6 0.8-2.2-0.3-2.7 0.1-2.1-0.7-2.9 0.5-2.1 0.9-1.3-0.2-2.2-1.5-1.1-1.6-0.2 0-3.1-0.3-0.6 1.7 0-1.7-2.8 3.2-5.3 1.1 0.3 0.8 2.1 3.4-1.2z"
                 id="AL" name="Albania">
             </path>
