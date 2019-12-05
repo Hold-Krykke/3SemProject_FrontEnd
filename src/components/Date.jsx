24f5bde1a6f2fd86cc182e6ee1e8f451999b9ac9
aborkhovych <a.borkhovych@gmail.com>
2019-12-05 21:47:12 +0200

    actual changes

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker"; // Required for Date Picker to work.
-import "react-datepicker/dist/react-datepicker.css"; // Required for Date Picker to work.
-import "react-datepicker/dist/react-datepicker-cssmodules.css"; // Required for Date Picker to work.
+// import "react-datepicker/dist/react-datepicker.css"; // Required for Date Picker to work.
+// import "react-datepicker/dist/react-datepicker-cssmodules.css"; // Required for Date Picker to work.
 import { Modal, CardDeck, Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from './DateStyles.css'
 
 // Text or Formats used multiple places throughout code.
 const myDateFormat = "yyyy/MM/dd";
